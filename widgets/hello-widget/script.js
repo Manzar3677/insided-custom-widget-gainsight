8b28f57242263614c0b1ee0a40710bee33e70dac
Manzar Hussain <42630927+Manzar3677@users.noreply.github.com>
2026-03-12 15:37:46 +0530

    Update script.js

--- widgets/hello-widget/script.js
+++ widgets/hello-widget/script.js
@@ -11,3 +11,15 @@ function initWidget() {
 }
 
 initWidget();
+
+  (async () => {
+    const sdk = new window.WidgetServiceSDK();
+
+    const result = await sdk.connectors.execute({
+      permalink: "users-location",
+      method: "GET"
+    });
+
+    console.log(result);
+  })();
+
